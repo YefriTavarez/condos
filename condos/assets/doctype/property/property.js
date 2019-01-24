f4317593d9ca492033da8ab59ab8d7d00fd86723
Yefri Tavarez <yefritavarez@hotmail.com>
2019-01-24 00:54:30 -0400

    Add Navigate to the Property button

--- condos/assets/doctype/property/property.js
+++ condos/assets/doctype/property/property.js
@@ -2,7 +2,58 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Property', {
-	refresh: function(frm) {
+	refresh: frm => {
+		const events = [
+			"add_custom_buttons",
+		];
 
+		$.map(
+			events,
+			frm
+				.trigger
+				.bind(frm)
+		);
+	},
+	add_custom_buttons: frm => {
+		const events = [
+			"add_navigate_button",
+		];
+
+		$.map(
+			events,
+			frm
+				.trigger
+				.bind(frm)
+		);
+	},
+	add_navigate_button: frm => {
+		const {
+			doc,
+		} = frm;
+
+		if (doc.geolocation) {
+			frm.add_custom_button(
+				__("Navigate to Location"),
+				event => frm
+					.trigger("navigate_to_geolocation")
+			);
+		}
+	},
+	navigate_to_geolocation: frm => {
+		const { doc, } = frm,
+			geolocation =
+				JSON.parse(doc.geolocation);
+
+		return geolocation
+			.features
+			.map(feature => {
+				// console.table(feature);
+				const coordinates = feature
+					.geometry
+					.coordinates;
+
+				// http://maps.google.com/?q=[lat],[long]
+				open(`http://maps.google.com/?q=${coordinates[1]},${coordinates[0]}`);
+			});
 	}
 });
